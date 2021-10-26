6574af75df29c5563e4a35efa366d5276c70cbad
muskan89 <muskangupta1107@gmail.com>
2021-10-26 10:58:28 +0530

    sample schemas

--- test.sql
+++ test.sql
@@ -1,6 +1,6 @@
 CREATE TABLE CourseCatalogue
 (
-Code varchar(255),
+Course_id varchar(255),
 Title varchar(255),
 --LTPSC  varchar(255)
 Lecture int,
@@ -12,7 +12,7 @@ Credit int
 
 CREATE TABLE PreRequisite
 (
-Code varchar(255),
+Course_id varchar(255),
 preRequisite_course_code varchar(255),
 foreign key (Code, preRequisite_course_code) references CourseCatalogue(code)
 );
@@ -21,20 +21,20 @@ foreign key (Code, preRequisite_course_code) references CourseCatalogue(code)
 --Course should be present in course catalogue
 CREATE TABLE CourseOfferings
 (
-Code varchar(255),
+Course_id varchar(255),
 Title varchar(255),
 semester int,
 Instructor varchar(255),
 LTPSC  varchar(255),
 --Batch allowed
 --timeslot
-cgConstraint int
+cgConstraint int,
 foreign key (Code, Title) references CourseCatalogue(Code,Title)
 );
 
 CREATE TABLE TimeSlot
 (
-Code varchar(255),
+Course_id varchar(255),
 Duration varchar(255),
 startingTime varchar(255),
 endingTime varchar(255),
@@ -54,6 +54,25 @@ CREATE TABLE Student
     yearOfAdmission int,
     dept_name varchar(255),
     total_credit int 
+);
+
+CREATE TABLE isGoingToTake
+(
+    entry_num varchar(255),
+    Course_id varchar(255), 
+    Sec_id int,
+    year int,
+    semester int, 
+);
+
+CREATE TABLE historyStudent
+(
+    entry_num varchar(255),
+    Course_id varchar(255), 
+    Sec_id int,
+    year int,
+    semester int, 
+    
 );
 
         
