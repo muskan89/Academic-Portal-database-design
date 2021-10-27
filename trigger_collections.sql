f1e618c522141c9919d82f14167cff6ca92c3d64
muskan89 <muskangupta1107@gmail.com>
2021-10-27 13:04:52 +0530

    triggers

--- trigger_collections.sql
+++ trigger_collections.sql
@@ -13,11 +13,14 @@ Credit int
 CREATE TABLE isGoingToTake
 (
     entry_num varchar(255),
-    Course_id varchar(255), 
+    Course_id varchar(255),
+    credit int, 
     Sec_id int,
     year int,
     semester int, 
 );
+
+
 
 --Course should be present in course catalogue
 CREATE TABLE CourseOfferings
@@ -65,6 +68,7 @@ Course_id varchar(255),
 preRequisite_course_code varchar(255),
 foreign key (Code, preRequisite_course_code) references CourseCatalogue(code)
 );
+
 CREATE TABLE historyOfStudent
 (
     entry_num varchar(255),
@@ -75,6 +79,98 @@ CREATE TABLE historyOfStudent
     department varchar(255),
     yearOfAdmission int
 );
+
+CREATE TABLE studentsTicketRequest
+(
+    entry_num varchar(255),
+    sem int,
+    Course_id varchar(255), 
+    facultyPermission int,
+    BatchAdvisorPermission int,
+    DeanPermission int
+);
+
+CREATE TABLE facultyTicketRequest
+(
+    entry_num varchar(255),
+    sem int,
+    Course_id varchar(255), 
+    facultyPermission int,
+    BatchAdvisorPermission int,
+    DeanPermission int
+);
+
+CREATE TABLE BatchAdvisorTicketRequest
+(
+    entry_num varchar(255),
+    sem int,
+    Course_id varchar(255), 
+    facultyPermission int,
+    BatchAdvisorPermission int,
+    DeanPermission int
+);
+
+CREATE TABLE DeanTicketRequest
+(
+    entry_num varchar(255),
+    sem int,
+    Course_id varchar(255), 
+    facultyPermission int,
+    BatchAdvisorPermission int,
+    DeanPermission int
+);
+
+
+CREATE OR REPLACE FUNCTION TriggerGenerator(entry_no varchar(255),course_id varchar(255))
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+--declared something
+_unit record;
+_slot record;
+flag integer := 0;
+BEGIN
+for _unit in (select * from  TimeSlot where TimeSlot.Course_id=course_id and TimeSlot.Course_id in (select isGoingToTake.Course_id from isGoingToTake where entry_no=isGoingToTake.entry_num))
+loop
+    for _slot in (select * in TimeSlot where TimeSlot.Course_id!=course_id and TimeSlot.Course_id in (select isGoingToTake.Course_id from isGoingToTake where entry_no=isGoingToTake.entry_num))
+    loop
+        --condition we will have to think
+        if(_unit.Duration == _slot.Duration and _unit.startingTime==_slot.startingTime and _unit.endingTime==_slot.endingTime)
+        then flag := flag + 1;
+        ELSE
+        THEN
+            raise exception 'time slot % is clashing with other courses',_unit;
+    end loop;
+end loop;
+RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER clashes_time_slot
+Before INSERT
+ON isGoingToTake
+FOR EACH ROW
+EXECUTE PROCEDURE check_clashes_time_slot();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 CREATE OR REPLACE FUNCTION check_clashes_time_slot(entry_no varchar(255),course_id varchar(255))
@@ -174,7 +270,7 @@ DECLARE
 yr integer := 0;
 dep varchar(255);
 _unit record;
-falg integer := 1; 
+flag integer := 1; 
 BEGIN
     dep := (select Student.dept_name from Student where Student.entry_num=entry_no);
     yr := (select Student.yearOfAdmission from Student where Student.entry_num=entry_no);
@@ -200,36 +296,58 @@ EXECUTE PROCEDURE batch_criteria_checking();
 
 --trigger to check if the course, student going to insert is fulfilling the credit limit this semester or not
 
-CREATE OR REPLACE FUNCTION batch_criteria_checking(entry_no int,sem int)
+CREATE OR REPLACE FUNCTION credit_limit_checking(entry_no int,thisSem int,course_id varchar(255))
 RETURNS TRIGGER
 LANGUAGE PLPGSQL
 AS $$
 DECLARE
-yr integer := 0;
-dep varchar(255);
+checking integer := 0;
+credit1 integer := 0;
+credit2 integer := 0;
+credit3 integer := 0;
+
 _unit record;
-falg integer := 1; 
+ 
 BEGIN
-    dep := (select Student.dept_name from Student where Student.entry_num=entry_no);
-    yr := (select Student.yearOfAdmission from Student where Student.entry_num=entry_no);
-    for _unit in (select * from BatchesAllowed where BatchesAllowed.Course_id=course_id)
+    for _unit in (select unit from historyOfStudent where historyOfStudent.entry_num=entry_no and historyOfStudent.sem=(thisSem-1))
     loop
-        if dep == _unit.dept_name and yr == _unit.yearOfAdmission
-        then flag := flag + 1;
-        end if;
-    end loop;
-    if flag == 0
-    then raise exception 'student has not fulfilled batch-criteria of the course';
+    if(_unit.grade >= 4)
+    then credit1 := _unit.credit + credit1;
     end if;
+
+    end loop;
+
+    for _unit in (select unit from historyOfStudent where historyOfStudent.entry_num=entry_no and historyOfStudent.sem=(thisSem-2))
+    loop
+    if(_unit.grade >= 4)
+    then credit2 := _unit.credit + credit2;
+    end if;
+    end loop;
+
+    for _unit in (select unit from isGoingToTake where isGoingToTake.entry_num=entry_no and isGoingToTake.sem=(thisSem-2))
+    loop
+    if(_unit.grade >= 4)
+    then credit3 := _unit.credit + credit3;
+    end if;
+    end loop;
+
+    credit3 := credit3 + (select CourseCatalogue.Credit from CourseCatalogue where CourseCatalogue.Course_id=course_id);
+    checking := (credit1 + credit2)/2;
+    checking := checking * 1.25;
+    if(credit3 > checking)
+    THEN raise exception 'credit limit will increase after taking this course';
+    endif;
+
+
 RETURN NEW;
 END;
 $$;
 
-CREATE TRIGGER batch_criteria_handler
+CREATE TRIGGER credit_limit_handler
 Before INSERT
 ON isGoingToTake
 FOR EACH ROW
-EXECUTE PROCEDURE batch_criteria_checking();
+EXECUTE PROCEDURE credit_limit_checking();
 
 
 
